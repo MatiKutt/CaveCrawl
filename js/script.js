4629d3b9796560243e195b79c2cc39801eff8b7f
MatiKutt <58309985+MatiKutt@users.noreply.github.com>
2021-04-25 21:42:13 +0100

    added pw

--- js/script.js
+++ js/script.js
@@ -117,7 +117,17 @@ function updateScore() {
 	
 	
 
+function onIndexLoad()
+{
+	
+	'use strict';
+	if ('serviceWorker' in navigator) {
+      navigator.serviceWorker
+               .register('./service_worker.js');
+			   console.log("register function");
+    }
 
+}
 
 
 
